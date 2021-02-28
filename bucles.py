4b1bdf0c0812d5a053a63ea9bcf40227c269771d
Lender Lucas <lenderlucas20@hotmail.com>
2021-02-28 15:44:35 -0500

    Se agregan algunos programas al repositorio

---
+++ bucles.py
@@ -0,0 +1,46 @@
+# def run():
+#     #LIMITE = 1000
+#     limite_con =int(input('Ingresa el numero a elevar:  '))
+#     limite =int(input('Ingresa el valor a elevar:  '))
+#     contador=0
+#     potencia_2= limite_con**contador
+
+#     while potencia_2 < limite:
+#         print(str(limite_con)+ ' elevado a '+ str(contador)+ ' es igual a = '+ str(potencia_2))
+#         contador += 1
+#         potencia_2 = limite_con**contador
+
+
+
+
+# if __name__ == "__main__":
+#     run()
+
+# contador = 0
+# for contador in range(1,1000):
+#     contador += 1
+#     print(contador)
+def es_primo(numero):
+    contador=0
+
+    for i in range(1, numero):
+        if i==1 or i==numero:
+            continue
+        if numero % i ==0:
+            contador+=1
+    if contador==0:
+        return True
+    else:
+        return False
+
+
+
+def run():
+    numero =int(input('Escibre un numero para evaluar si es primo o no: '))
+    if es_primo(numero):
+        print('El numero que ingresaste es primo')
+    else:
+        print('El numero que ingresaste no es primo')
+
+if __name__ == "__main__":
+    run()
